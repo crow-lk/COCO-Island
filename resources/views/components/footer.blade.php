4b273ac38a98547456aab4e8df5e5f014b520c29
Wasanjith Kodikara <149646160+wasanjith@users.noreply.github.com>
2025-08-29 16:13:38 +0530

    devide into components file the index file

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,128 @@
+<!-- footer area start -->
+<footer class="footer-area grey-bg-2">
+    <div class="footer-top pt-95 pb-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                    <div class="footer-widget mb-50">
+                        <div class="footer-title">
+                            <h3>Get in Touch</h3>
+                        </div>
+                        <div class="footer-content">
+                            <div class="footer-contact">
+                                <p>Lorem ipsum dolor sit am co adipisicing elit, sed do eiusmod tempor</p>
+                                <ul>
+                                    <li>
+                                        <div class="footer-contact-inner d-flex align-items-center">
+                                            <div class="icon mr-20">
+                                                <span><i class="fas fa-map-marker-alt"></i></span>
+                                            </div>
+                                            <div class="text">
+                                                <p>01, Wickramasinghepura, Ruwanmaga, Dodangoda, Kaluthara</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="footer-contact-inner d-flex align-items-center">
+                                            <div class="icon mr-20">
+                                                <span><i class="fas fa-phone-alt"></i></span>
+                                            </div>
+                                            <div class="text">
+                                                <p>+94 77 660 5054</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="footer-contact-inner d-flex align-items-center">
+                                            <div class="icon mr-20">
+                                                <span><i class="fal fa-envelope"></i></span>
+                                            </div>
+                                            <div class="text">
+                                                <p> yourmail@gmail.com</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-6 col-sm-6 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-title">
+                            <h3>Follow Us</h3>
+                        </div>
+                        <div class="footer-content">
+                            <div class="follow-us">
+                                <ul>
+                                    <li><a href="https://www.facebook.com/share/15CgQahxzH/?mibextid=wwXIfr" target="_blank">Facebook </a></li>
+                                    <li><a href="https://www.instagram.com/cocoislandholidaysofficial?igsh=MWo5aHk3aDBoOWlxOA%3D%3D&utm_source=qr" target="_blank">Instagram</a></li>
+                                    <li><a href="https://www.tiktok.com/@coco.island.holidays?_t=ZS-8z223emMSIq&_r=1" target="_blank">TikTok</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 offset-xl-1 col-lg-5 col-md-6 col-sm-6">
+                    <div class="footer-widget mb-50">
+                        <div class="footer-title">
+                            <h3>Subscribe</h3>
+                        </div>
+                        <div class="footer-content">
+                            <div class="subscribe">
+                                <p>Lorem ipsum dolor si  consectetur adipisicing elit, sed eiusmotempor incididunt ut labore et</p>
+                                
+                                <div class="subscribe-form">
+                                    <form action="#">
+                                        <input type="email" placeholder="Type Your Email">
+                                        <button class="sub-btn" type="submit"><i class="fas fa-location-arrow"></i></button>
+                                    </form>
+                                </div>
+                                <div class="social">
+                                    <ul>
+                                        <li>
+                                            <a href="https://www.facebook.com/share/15CgQahxzH/?mibextid=wwXIfr" target="_blank">
+                                                <i class="fab fa-facebook-f social-icon-lg"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.instagram.com/cocoislandholidaysofficial?igsh=MWo5aHk3aDBoOWlxOA%3D%3D&utm_source=qr" target="_blank">
+                                                <i class="fab fa-instagram social-icon-lg"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.tiktok.com/@coco.island.holidays?_t=ZS-8z223emMSIq&_r=1" target="_blank">
+                                                <i class="fab fa-tiktok social-icon-lg"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="footer-copyright-inner">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-12">
+                        <div class="copyright-text">
+                            <p>Copyright © {{ date('Y') }} <a href="https://cocoislandholidays.com" target="_blank">COCO Island holiday</a>. All Rights Reserved by <a href="https://crow.lk" target="_blank">Crow.lk</a></p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-12">
+                        <div class="footer-policy ">
+                            <a href="#">Terms & Condition</a>
+                            <a href="#"> Privacy</a>
+                            <a href="#">Support</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+</footer>
+<!-- footer area end -->
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,82 @@
+<!-- header area start -->
+<header class="header-area header-transparent">
+    <div class="header-top">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-5 col-6">
+                    <div class="header-top-left">
+                        <span class="d-none d-sm-inline-block"><i class="fas fa-phone-alt"></i>+94 77 660 5054</span>
+                        <span class="d-none d-lg-inline-block"><i class="fas fa-envelope"></i>Demo@gmail.com</span>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-7 col-6">
+                    <div class="header-top-right d-flex justify-content-end align-items-center">
+                        <div class="header-lang d-none d-sm-inline-block">
+                            <span>English</span>
+                        </div>
+                        <div class="header-currency d-none d-sm-inline-block">
+                            <select name="header-currency">
+                                <option value="1">$ USD</option>
+                                <option value="2">$ USD</option>
+                                <option value="3">$ USD</option>
+                                <option value="4">$ USD</option>
+                                <option value="5">$ USD</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6 col-6">
+                    <div class="logo">
+                        <a href="{{ route('home') }}"><img src="{{ asset('frontend/assets/img/logo/coco-logo.png') }}" alt="COCO Island Holidays Logo"></a>
+                    </div>
+                </div>
+                <div class="col-xl-9 offset-xl-1 col-lg-10 col-md-8 col-sm-6 col-6">
+                    <div class="header-bottom-right d-flex justify-content-end align-items-center">
+                        <div class="main-menu">
+                            <nav id="mobile-menu">
+                                <ul>
+                                    <li><a href="{{ route('home') }}" class="arrow"> Home</a>
+                                        <ul class="submenu">
+                                            <li><a href="#">Submenu 1</a></li>
+                                            <li><a href="#">Submenu 2</a></li>
+                                            <li><a href="#">Submenu 3</a></li>
+                                            <li><a href="#">Submenu 4</a></li>
+                                            <li><a href="#">Submenu 5</a></li>
+                                            <li><a href="#">Submenu 6</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Packages</a></li>
+                                    <li><a href="#">Hotels</a></li>
+                                    <li><a href="#">Jobs</a></li>
+                                    <li><a href="#" class="arrow">NEWS</a>
+                                        <ul class="submenu">
+                                            <li><a href="{{ route('blog.details') }}">News Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Life</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="search-box">
+                            <a class="search" href="javascript:void(0)"><i class="fas fa-search"></i></a>
+                        </div>
+                        <div class="header-btn ml-45 d-none d-sm-inline-block">
+                            <a href="#" class="btn">Book Online</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="mobile-menu"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- header area end -->
