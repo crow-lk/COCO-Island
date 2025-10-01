6fe9a1d481ef5e6548f8d38455c3e1c9979a4402
Kavindu Lakshitha <kavindu.lakshitha.ofc@gmail.com>
2025-10-01 23:01:29 +0530

    Google analytics setup done.

--- resources/views/modern-contact.blade.php
+++ resources/views/modern-contact.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}" class="scroll-smooth">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q5TC30C9BX"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-Q5TC30C9BX');
+    </script>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us | COCO Island Holidays - Travel Agency</title>
